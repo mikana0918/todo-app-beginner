bf811d59d8aa19d64fdf762eeb7277d30f50755c
mikana0918 <shogo.kusuhara@gmail.com>
2024-02-03 18:58:00 +0900

    refactor: Refactor TodoControllerTest to include test for listing todos

--- tests/Feature/TodoControllerTest.php
+++ tests/Feature/TodoControllerTest.php
@@ -7,10 +7,34 @@ use Tests\TestCase;
 
 class TodoControllerTest extends TestCase
 {
-    public function test_should_list_todos(): void
+    public function test_should_list_empty_todos(): void
     {
         $response = $this->get('/api/todos');
 
         $response->assertStatus(200);
+    }
+
+    public function test_should_list_todos(): void
+    {
+        $completed_todo_1 = Todo::factory()->completed()->create();
+        $completed_todo_2 = Todo::factory()->completed()->create();
+        $uncompleted_todo_1 = Todo::factory()->uncompleted()->create();
+
+
+        $response = $this->get('/api/todos');
+
+        $response->assertStatus(200)->assertJsonFragment([
+            'id' => $completed_todo_1->id,
+            'title' => $completed_todo_1->title,
+            'is_completed' => true,
+        ])->assertJsonFragment([
+            'id' => $completed_todo_2->id,
+            'title' => $completed_todo_2->title,
+            'is_completed' => true,
+        ])->assertJsonFragment([
+            'id' => $uncompleted_todo_1->id,
+            'title' => $uncompleted_todo_1->title,
+            'is_completed' => false,
+        ]);
     }
 }
